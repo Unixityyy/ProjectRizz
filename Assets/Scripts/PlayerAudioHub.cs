c437836cdc859dc14c0d848983fd04342c96a4e1
Shannja <shannuel22m@gmail.com>
2026-03-06 15:52:01 -0600

    upd 1.5.5 and 1.5.51

--- Assets/Scripts/PlayerAudioHub.cs
+++ Assets/Scripts/PlayerAudioHub.cs
@@ -5,18 +5,27 @@ public class PlayerAudioHub : MonoBehaviourPunCallbacks
 {
     public AudioSource soundboardSource;
     public AudioClip[] clips;
+    public float cooldownTime = 0.5f;
+    private float lastPlayedTime;
 
     public void RequestSound(int index)
     {
-        photonView.RPC("RPC_PlaySound", RpcTarget.All, index);
+        if (photonView.IsMine && Time.time >= lastPlayedTime + cooldownTime)
+        {
+            lastPlayedTime = Time.time;
+            photonView.RPC("RPC_PlaySound", RpcTarget.All, index);
+        }
     }
 
     [PunRPC]
     void RPC_PlaySound(int index)
     {
-        if (index >= 0 && index < clips.Length)
+        if (clips != null && index >= 0 && index < clips.Length)
         {
-            soundboardSource.PlayOneShot(clips[index]);
+            if (clips[index] != null)
+            {
+                soundboardSource.PlayOneShot(clips[index]);
+            }
         }
     }
 }
