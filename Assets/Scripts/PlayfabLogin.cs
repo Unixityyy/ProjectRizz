6efb93c10d6f8610ad4338e774c38db99757a14f
Unixity <103908889+Unixityyy@users.noreply.github.com>
2026-03-28 09:06:32 -0700

    forgot to commit

--- Assets/Scripts/PlayfabLogin.cs
+++ Assets/Scripts/PlayfabLogin.cs
@@ -13,8 +13,6 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
-using Photon.Voice;
-using Photon.Voice.Unity;
 using Photon.Voice.PUN;
 
 public class BypassCertificate : CertificateHandler
@@ -28,7 +26,7 @@ public class BypassCertificate : CertificateHandler
 public class PlayfabLogin : MonoBehaviourPunCallbacks
 {
     [Header("SETTINGS")]
-    public bool useAttestation = false;
+    public string backendUrl = "https://api.unixityyy.dev/api/v1/attestation/secure-login";
 
     [Header("COSMETICS")]
     public static PlayfabLogin instance;
@@ -38,39 +36,38 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
     public GameObject FlingButtons;
     public List<GameObject> specialitems;
     public List<GameObject> disableitems;
+
     [Header("CURRENCY")]
     public string CurrencyName;
     public TextMeshPro currencyText;
-    [SerializeField]
     public int coins;
+
     [Header("BANNED")]
     public string bannedscenename;
+
     [Header("TITLE DATA")]
     public GameObject ModApps;
     public TextMeshPro MOTDText;
+
     [Header("PLAYER DATA")]
     public TextMeshPro UserName;
-    [SerializeField] public bool UpdateName;
-    public string StartingUsername;
     public string Name;
     public TextMeshPro IDText;
     public string OculusID;
     public string OculusUserName;
     public string OculusDisplayName;
+
     [Header("MISC")]
     public UnityEvent LoginEvent = new UnityEvent();
-    public bool MetaAuthSuceed;
     public TextMeshPro CurrentServer;
-    private string playFabTicket;
     private bool hashed;
     private bool UsernameGot;
     private bool photonMetaSynced = false;
-    private string backendUrl = "https://dzhkj7xsbckrhwodmsxllguc2i0cxxqs.lambda-url.us-east-1.on.aws/";
+    private bool MetaAuthSuceed;
 
     public void ModCall(string Reason)
     {
         string jsonPayload = "{\"content\": \"<@&1355942327583248494> Player has called for mod! Reason: " + Reason + ", Player ID is " + MyPlayFabID + ".\", \"allowed_mentions\": {\"roles\": [\"1355942327583248494\"]}}";
-
         UnityWebRequest www = new UnityWebRequest("https://discord.com/api/webhooks/1413736708775612416/OWgCe3UTPiphoKyLwb0miqhIbz0brl_jJ8_0Ks-Qr6MgjZaocMTJlL3LqkUk4rS_2bYm", "POST");
         byte[] jsonToSend = new UTF8Encoding().GetBytes(jsonPayload);
         www.uploadHandler = new UploadHandlerRaw(jsonToSend);
@@ -81,7 +78,7 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
         {
             if (www.result != UnityWebRequest.Result.Success)
             {
-                Debug.LogError("Reporting Webhook Error: " + www.error);
+                Debug.LogError("Webhook Error: " + www.error);
             }
         };
     }
@@ -100,6 +97,12 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
         {
             Users.GetLoggedInUser().OnComplete(GetLoggedInUserCallback);
         }
+        else
+        {
+            Core.AsyncInitialize().OnComplete(m => {
+                if (!m.IsError) Users.GetLoggedInUser().OnComplete(GetLoggedInUserCallback);
+            });
+        }
     }
 
     private void GetLoggedInUserCallback(Message<User> msg)
@@ -113,66 +116,44 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
         }
         else
         {
-            MOTDText.text = "FAILED TO AUTHENTICATE WITH META!";
+            MOTDText.text = "AUTH FAILED";
         }
-    }
-
-    void LoginWithCustomID()
-    {
-        var request = new LoginWithCustomIDRequest
-        {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
-        };
-        PlayFabClientAPI.LoginWithCustomID(request, OnCustomIDLoginSuccess, OnError);
     }
 
     private void OnEntitlementCheckComplete(Message msg)
     {
         if (msg.IsError)
         {
-            MOTDText.text = "NOT ENTITLED TO APP!";
+            MOTDText.text = "NOT ENTITLED";
         }
         else
         {
             MetaAuthSuceed = true;
-            LoginWithCustomID(); 
+            StartSecureAttestation();
         }
     }
 
-    private void OnCustomIDLoginSuccess(LoginResult result)
+    private void StartSecureAttestation()
     {
-        playFabTicket = result.SessionTicket;
-        MyPlayFabID = result.PlayFabId;
-
-        if (useAttestation)
-        {
 #if UNITY_STANDALONE_ANDROID && !UNITY_EDITOR
-            string nonce = System.Guid.NewGuid().ToString();
-
-            Oculus.Platform.DeviceApplicationIntegrity.GetIntegrityToken(nonce).OnComplete(message => {
-                if (!message.IsError) {
-                    string integrityToken = message.Data;
-                    Debug.Log("got integ token");
-                    StartCoroutine(SecureLoginRoutine(integrityToken));
-                } else {
-                    var error = message.GetError();
-                    Debug.LogError($"integ err: {error.Code} - {error.Message}");
-                    MOTDText.text = "SECURITY CHECK FAILED";
-                }
-            });
-#elif UNITY_EDITOR
-            HandlePostLogin();
+        string nonce = System.Guid.NewGuid().ToString();
+        Oculus.Platform.DeviceApplicationIntegrity.GetIntegrityToken(nonce).OnComplete(message => {
+            if (!message.IsError) {
+                StartCoroutine(SecureLoginRoutine(message.Data));
+            } else {
+                MOTDText.text = "ATTESTATION FAILED";
+                Debug.LogError($"Integ err: {message.GetError().Message}");
+            }
+        });
+#else
+        MOTDText.text = "EDITOR MODE";
 #endif
-        } else
-        {
-            HandlePostLogin();
-        }
     }
 
     IEnumerator SecureLoginRoutine(string attestationToken)
     {
-        string jsonPayload = "{\"attestation_token\":\"" + attestationToken + "\", \"playfab_id\":\"" + MyPlayFabID + "\"}";
+        string hwId = SystemInfo.deviceUniqueIdentifier;
+        string jsonPayload = "{\"attestationToken\":\"" + attestationToken + "\", \"hardwareId\":\"" + hwId + "\"}";
         byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonPayload);
 
         using (UnityWebRequest request = new UnityWebRequest(backendUrl, "POST"))
@@ -180,25 +161,26 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
             request.uploadHandler = new UploadHandlerRaw(bodyRaw);
             request.downloadHandler = new DownloadHandlerBuffer();
             request.SetRequestHeader("Content-Type", "application/json");
+            request.certificateHandler = new BypassCertificate();
 
             yield return request.SendWebRequest();
 
             if (request.result == UnityWebRequest.Result.Success)
             {
-                Debug.Log("attestation success");
+                var response = JsonUtility.FromJson<SecureLoginResponse>(request.downloadHandler.text);
+                
+                PlayFabSettings.staticPlayer.ClientSessionTicket = response.sessionTicket;
+                MyPlayFabID = response.playFabId;
 
                 HandlePostLogin();
             }
             else
             {
-                MOTDText.text = "META HORIZON ATTESTATION HAS FAILED. PLEASE RESTART THE GAME. IF THIS KEEPS HAPPENING REINSTALL THE GAME.";
-                Debug.LogError("attestation fail: " + request.error);
+                MOTDText.text = "LOGIN FAILED";
+                Debug.LogError("Attestation Error: " + request.error);
             }
         }
     }
-
-    public override void OnCreateRoomFailed(short returnCode, string message) => Debug.LogError($"Create Room Failed: {message}");
-    public override void OnJoinRoomFailed(short returnCode, string message) => Debug.LogError($"Join Room Failed: {message}");
 
     private void HandlePostLogin()
     {
@@ -211,8 +193,7 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
             string username = result.AccountInfo.TitleInfo.DisplayName;
             if (string.IsNullOrEmpty(username))
             {
-                string numbers = Random.Range(0, 9999).ToString("D4");
-                string newUsername = "RIZZ" + numbers;
+                string newUsername = "RIZZ" + Random.Range(0, 9999).ToString("D4");
                 PlayFabClientAPI.UpdateUserTitleDisplayName(new UpdateUserTitleDisplayNameRequest { DisplayName = newUsername }, null, null);
                 PhotonVRManager.SetUsername(newUsername);
             }
@@ -228,57 +209,17 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
         }, ConnectToPhoton, OnPlayFabError);
     }
 
-    public override void OnJoinedRoom()
-    {
-        // 99.99% is connected and ready but a little check never hurt nobody
-        if (PhotonNetwork.IsConnectedAndReady && UsernameGot)
-        {
-            ExitGames.Client.Photon.Hashtable customProps = new ExitGames.Client.Photon.Hashtable();
-            
-            customProps["MetaUsername"] = OculusUserName;
-            customProps["MetaDisplayName"] = OculusDisplayName;
-            
-            PhotonNetwork.LocalPlayer.SetCustomProperties(customProps);
-        }
-        CurrentServer.text = $"CURRENT SERVER: {PhotonNetwork.CurrentRoom.Name}";
-    }
-
-    public override void OnLeftRoom()
-    {
-        CurrentServer.text = $"CURRENT SERVER: NONE";
-    }
-
-
-    void UpdatePlayFabMetaInfo()
-    {
-        var request = new UpdateUserDataRequest
-        {
-            Data = new Dictionary<string, string> {
-                { "MetaUsername", OculusUserName },
-                { "MetaDisplayName", OculusDisplayName }
-            }
-        };
-        PlayFabClientAPI.UpdateUserData(request, null, null);
-
-        UsernameGot = true;
-    }
-
     void ConnectToPhoton(GetPhotonAuthenticationTokenResult result)
     {
         if (MetaAuthSuceed && !string.IsNullOrEmpty(MyPlayFabID))
         {
             AuthenticationValues authValues = new AuthenticationValues();
             authValues.AuthType = CustomAuthenticationType.Custom;
-
             authValues.AddAuthParameter("username", MyPlayFabID);
             authValues.AddAuthParameter("token", result.PhotonCustomAuthenticationToken);
 
             PhotonNetwork.AuthValues = authValues;
-
-            if (PunVoiceClient.Instance != null)
-            {
-                PunVoiceClient.Instance.Client.AuthValues = authValues;
-            }
+            if (PunVoiceClient.Instance != null) PunVoiceClient.Instance.Client.AuthValues = authValues;
 
             PhotonNetwork.ConnectUsingSettings();
         }
@@ -297,8 +238,7 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
                 {
                     foreach (var sItem in specialitems) if (sItem.name == item.ItemId) sItem.SetActive(true);
                     foreach (var dItem in disableitems) if (dItem.name == item.ItemId) dItem.SetActive(false);
-                    if (item.ItemId == "Moderator") KickButtons.SetActive(true);
-                    if (item.ItemId == "Moderator") FlingButtons.SetActive(true);
+                    if (item.ItemId == "Moderator") { KickButtons.SetActive(true); FlingButtons.SetActive(true); }
                 }
             }
         }, OnError);
@@ -308,15 +248,20 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
     {
         PlayFabClientAPI.GetTitleData(new GetTitleDataRequest(), result =>
         {
-            if (result.Data != null && result.Data.ContainsKey("MOTD"))
-            {
-                MOTDText.text = result.Data["MOTD"];
-            }
-            if (result.Data.ContainsKey("ModApps"))
-            {
-                ModApps.SetActive(bool.TryParse(result.Data["ModApps"], out bool isEnabled) && isEnabled);
-            }
+            if (result.Data != null && result.Data.ContainsKey("MOTD")) MOTDText.text = result.Data["MOTD"];
+            if (result.Data.ContainsKey("ModApps")) ModApps.SetActive(bool.TryParse(result.Data["ModApps"], out bool isEnabled) && isEnabled);
         }, OnError);
+    }
+
+    void UpdatePlayFabMetaInfo()
+    {
+        PlayFabClientAPI.UpdateUserData(new UpdateUserDataRequest {
+            Data = new Dictionary<string, string> {
+                { "MetaUsername", OculusUserName },
+                { "MetaDisplayName", OculusDisplayName }
+            }
+        }, null, null);
+        UsernameGot = true;
     }
 
     private void Update()
@@ -335,24 +280,35 @@ public class PlayfabLogin : MonoBehaviourPunCallbacks
             ExitGames.Client.Photon.Hashtable metaHash = new ExitGames.Client.Photon.Hashtable();
             metaHash["MetaUsername"] = OculusUserName;
             metaHash["MetaDisplayName"] = OculusDisplayName;
-            
             PhotonNetwork.LocalPlayer.SetCustomProperties(metaHash);
             photonMetaSynced = true;
         }
     }
 
-    private void OnError(PlayFabError error)
+    public override void OnJoinedRoom()
     {
-        if (error.Error == PlayFabErrorCode.AccountBanned)
-            SceneManager.LoadScene(bannedscenename);
+        if (PhotonNetwork.IsConnectedAndReady && UsernameGot)
+        {
+            ExitGames.Client.Photon.Hashtable customProps = new ExitGames.Client.Photon.Hashtable();
+            customProps["MetaUsername"] = OculusUserName;
+            customProps["MetaDisplayName"] = OculusDisplayName;
+            PhotonNetwork.LocalPlayer.SetCustomProperties(customProps);
+        }
+        CurrentServer.text = $"CURRENT SERVER: {PhotonNetwork.CurrentRoom.Name}";
     }
 
+    public override void OnLeftRoom() => CurrentServer.text = $"CURRENT SERVER: NONE";
+
+    private void OnError(PlayFabError error)
+    {
+        if (error.Error == PlayFabErrorCode.AccountBanned) SceneManager.LoadScene(bannedscenename);
+    }
     private void OnPlayFabError(PlayFabError error) => Debug.LogError(error.GenerateErrorReport());
 
     [System.Serializable]
     public class SecureLoginResponse
     {
-        public string SessionTicket;
-        public string PlayFabId;
+        public string sessionTicket;
+        public string playFabId;
     }
 }
